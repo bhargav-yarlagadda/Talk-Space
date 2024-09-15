957a3a023a66ea62b84268ab68c8b8608a770b0b
bhargav yarlagadda <119490919+bhargav-yarlagadda@users.noreply.github.com>
2024-09-15 12:13:28 +0530

    feature/ connection is set up

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,17 +1,39 @@
-import React,{useState,useEffect} from 'react'
-import queryString from 'query-string'
-import io from 'socket.io-client'
-const Chat = ({location}) => {
-  useEffect(()=>{
-    const data = queryString.parse(location.search)
-    console.log(data)
-    console.log(location.search) 
-  },[])
+import React, { useEffect, useState } from 'react';
+import queryString from 'query-string';
+import { useLocation } from 'react-router-dom';
+import io from 'socket.io-client';
+
+let socket;
+
+const Chat = () => {
+  const location = useLocation();
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  "end point of sever"
+  const ENDPOINT = "http://localhost:5000";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    setName(name);
+    setRoom(room);
+    // Establish socket connection
+    socket = io(ENDPOINT);
+
+    // Clean up the socket connection on component unmount
+    socket.emit("join",{name,room},()=>{
+
+    })
+    return ()=>{
+      socket.emit('disconnect')
+      socket.off()
+    }
+  }, [ENDPOINT, location.search]);
+
   return (
     <div>
       Chat
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,33 +1,44 @@
-const express = require("express")
-const http = require("http")
-const socketio = require("socket.io")
-const app = express()
-const server = http.createServer(app)
-const router = require("./router")
-const io = socketio(server)
-const PORT = 5173 
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const cors = require("cors");
 
-io.on("connection",(socket)=>{
-    consolelog("user connected!")
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: "http://localhost:5173", // Allow requests from this origin
+    methods: ["GET", "POST"],
+    credentials: true // Allows the browser to include cookies with requests
+  }
+});
 
+const router = require("./router");
+const PORT = 5000;
 
-    socket.on("disconnect",()=>{
-        console.log("user disconnected")
+// Use CORS middleware for general HTTP routes
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("user connected!");
+  
+    socket.on("join",({name,room},callBack)=>{
+        console.log(name,room)
+        
     })
-})
 
 
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
+server.listen(PORT, (err) => {
+  if (err) {
+    console.log("Error while connecting to port", PORT);
+  } else {
+    console.log("Listening on port", PORT);
+  }
+});
 
-server.listen(PORT, (err)=>{
-    if(err){
-        console.log("error while connection to port",PORT)
-    }else{
-        console.log("listening on port ",PORT)
-    }
-} )
-
-
-app.use(router)
-
-
+app.use(router);
